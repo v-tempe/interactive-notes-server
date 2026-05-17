d5409638a9569453bafd1010b8dd05b598ae1918
Stepan Kutnenko <stepankutnenko@yandex.ru>
2026-05-18 01:57:10 +0300

    fix(build): add variable SSL_REQUIRE

--- interactive_notes/interactive_notes/settings.py
+++ interactive_notes/interactive_notes/settings.py
@@ -93,13 +93,14 @@ WSGI_APPLICATION = 'interactive_notes.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
+SSL_REQUIRE = os.environ.get('DJANGO_SSL_REQUIRE', '0') == '1'
 
 if DATABASE_URL:
     DATABASES = {
         'default': dj_database_url.config(
             default=DATABASE_URL,
             conn_max_age=600,
-            ssl_require=True,
+            ssl_require=SSL_REQUIRE,
         )
     }
 else:
