b72b25014694288cff88148b494f6ff774e8533f
Stepan Kutnenko <stepankutnenko@yandex.ru>
2026-06-25 15:18:07 +0300

    perf(core): improve power of gunicorn

--- interactive_notes/entrypoint.sh
+++ interactive_notes/entrypoint.sh
@@ -10,4 +10,4 @@ echo "Creating superuser"
 python init_admin.py
 
 echo "Starting Gunicorn"
-exec gunicorn interactive_notes.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn interactive_notes.wsgi:application --bind 0.0.0.0:8000 --timeout 120 --workers 3
